2c915976481cd829fa0bc93b5d065cfc48096dda
pawan779 <pawan.dharel77@gmail.com>
2019-12-06 14:53:59 +0545

    adapter class created

--- app/src/main/java/com/pawan/recycleview/contactAdapter.java
+++ app/src/main/java/com/pawan/recycleview/contactAdapter.java
@@ -1,13 +1,43 @@
 package com.pawan.recycleview;
 
+import android.content.Context;
+import android.provider.ContactsContract;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class contactAdapter {
+import java.util.List;
+
+public class contactAdapter extends RecyclerView.Adapter<contactAdapter.ContactsViewHolder>{
+
+Context mContext;
+List<contracts>contractsList;
+
+    public contactAdapter(Context mContext, List<contracts> contractsList) {
+        this.mContext = mContext;
+        this.contractsList = contractsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ContactsViewHolder extends RecyclerView.ViewHolder{
         ImageView imgProfile;
         TextView tvName,tvPhone;
