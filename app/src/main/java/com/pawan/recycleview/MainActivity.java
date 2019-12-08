34debb25d99b2209a48ee092f9b41e9813d0a14e
pawan779 <pawan.dharel77@gmail.com>
2019-12-08 20:03:59 +0545

    image shown in recycle view

--- app/src/main/java/com/pawan/recycleview/MainActivity.java
+++ app/src/main/java/com/pawan/recycleview/MainActivity.java
@@ -1,14 +1,30 @@
 package com.pawan.recycleview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
 
+public class MainActivity extends AppCompatActivity {
+private RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recycleView);
+
+        List<contracts> contractsList=new ArrayList<>();
+        contractsList.add(new contracts("Ronaldo","8324732784732",R.drawable.ronaldo));
+        contractsList.add(new contracts("Messi","3567432i483",R.drawable.messi));
+        contractsList.add(new contracts("Neymar","63646327",R.drawable.neymar));
+
+        contactAdapter contactAdapter=new contactAdapter(this,contractsList);
+        recyclerView.setAdapter(contactAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
--- app/src/main/java/com/pawan/recycleview/contactAdapter.java
+++ app/src/main/java/com/pawan/recycleview/contactAdapter.java
@@ -2,6 +2,7 @@ package com.pawan.recycleview;
 
 import android.content.Context;
 import android.provider.ContactsContract;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -20,22 +21,28 @@ List<contracts>contractsList;
     public contactAdapter(Context mContext, List<contracts> contractsList) {
         this.mContext = mContext;
         this.contractsList = contractsList;
+
     }
 
     @NonNull
     @Override
     public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view= LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_contracts,parent,false);
+        return new ContactsViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactsViewHolder holder, int position) {
+        contracts contracts=contractsList.get(position);
+        holder.imgProfile.setImageResource(contracts.getImageId());
+        holder.tvName.setText(contracts.getName());
+        holder.tvPhone.setText(contracts.getPhoneNo());
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return contractsList.size();
     }
 
     public class ContactsViewHolder extends RecyclerView.ViewHolder{
